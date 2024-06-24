ceca8e0d1ef604fadbe5bf74a2a0215b2fb0ee73
SCratORS <scrators@mail.ru>
2024-06-24 10:59:17 +0300

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,1 +1,2 @@
-g++ -O4  nes\*.cpp inih\*.c inih\cpp\*.cpp *.cpp  -o clover.exe -lmingw32 -lSDL2main -lSDL2 -mwindows
+taskkill /f /im clover.exe
+g++ -O4  nes\*.cpp LuPng\*.c inih\*.c inih\cpp\*.cpp *.cpp  -o clover.exe -lmingw32 -lSDL2main -lSDL2 -mwindows
